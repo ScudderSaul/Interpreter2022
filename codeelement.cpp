1cbe59edd1b109dce89cfc9a580f2995a53e653e
Saul Scudder <Saul.Scudder@gmail.com>
2022-08-17 12:51:28 -0700

    Update codeelement.cpp

--- codeelement.cpp
+++ codeelement.cpp
@@ -1,15 +1,4 @@
-//+---------------------------------------------------------------------------------------+
-//This file codeelement.cpp is copyright 1999 2000 2001 2002 2003 2004 2005 2006 2007 by Saul Scudder
-//It is distributed for non-profit use as freeware and no warranty is given with the use of
-//this source for any purpose whatsoever.
-//This copyright notice is to remain unmodified in the file
-//A license for commercial use of the _Zen_Soft programmers tools may be obtained by sending
-//a check for $10.00 to 
-//_Zen_Soft c/o 
-//Saul Scudder
-//358 E. Whitton
-//Phoenix AZ 85012
-//+---------------------------------------------------------------------------------------+
+
 
 #include "stdafx.h"
 #include <SYS\STAT.H> 
